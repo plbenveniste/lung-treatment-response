506b2b4ba75ce0c21c143e2c35faa4932000d8f6
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2024-06-06 16:46:19 -0400

    changed column names and remove \n

--- clinical_data_preprocessing.py
+++ clinical_data_preprocessing.py
@@ -61,6 +61,26 @@ def main():
     # We replace all '-' by nan in dataset
     data = data.apply(lambda x: x.replace('-', np.nan) if x.dtype == "object" else x)
 
+    # Renaming of the columns (removing \n)
+    change_columns = {
+        "num\n_patient": "'num_patient'",
+        "score\n_charlson": "score_charlson", 
+        "tabac\n_PA": "tabac_PA",
+        "tabac\n_sevre": "tabac_sevre",
+        "dose\n_tot": "dose_tot",
+        "vol\n_GTV": "vol_GTV",
+        "vol\n_PTV": "vol_PTV",
+        "vol\n_ITV": "vol_ITV",
+        "couv\n_PTV": "couv_PTV",
+        "BED\n_10": "BED_10",
+        "Date_R\n_PTV": "Date_R_PTV",
+        "Date_R\n_homo": "Date_R_homo",
+        "Date_R\n_med": "Date_R_med",
+        "Date_R\n_contro": "Date_R_contro",
+        "Date_R\n_horspoum": "Date_R_horspoum",
+    }
+    data = data.rename(columns=change_columns)
+
     # For each column we verify the data taken
     ## For sex we check that it can only take 0 or 1
     ## Convert the values from str to int
@@ -70,31 +90,31 @@ def main():
     ## For BMI we check that it's a float value
     data = data.apply(lambda x: x.str.replace(',', '.') if x.dtype == "object" else x)
     data['BMI'] = data['BMI'].apply(pd.to_numeric)
-    ## For score\n_charlson we check that they all take numeric values
-    data["score\n_charlson"] =  data["score\n_charlson"].apply(pd.to_numeric)
+    ## For score_charlson we check that they all take numeric values
+    data["score_charlson"] =  data["score_charlson"].apply(pd.to_numeric)
     ## For OMS we check that they all take numeric values
     data['OMS'] = data['OMS'].apply(pd.to_numeric)
-    ## For tabac, tabac\n_PA, tabac\n_sevre we check that they all take numeric values
+    ## For tabac, tabac_PA, tabac_sevre we check that they all take numeric values
     data['tabac'] = data['tabac'].apply(pd.to_numeric)
-    data['tabac\n_PA'] = data['tabac\n_PA'].apply(pd.to_numeric)
-    # data['tabac\n_sevre'] = data['tabac\n_sevre'].replace('7', '1')
-    data['tabac\n_sevre'] = data['tabac\n_sevre'].apply(pd.to_numeric)
+    data['tabac_PA'] = data['tabac_PA'].apply(pd.to_numeric)
+    # data['tabac_sevre'] = data['tabac_sevre'].replace('7', '1')
+    data['tabac_sevre'] = data['tabac_sevre'].apply(pd.to_numeric)
     ## For histo we check that they all take numeric values
     data['histo'] = data['histo'].apply(pd.to_numeric)
     ## For T we check that they all take numeric values
     data['T'] = data['T'].apply(pd.to_numeric)
     ## For centrale we check that they all take numeric values
     data['centrale'] = data['centrale'].apply(pd.to_numeric)
-    ## For dose\n_tot we check that they all take numeric values
-    data['dose\n_tot'] = data['dose\n_tot'].apply(pd.to_numeric)
+    ## For dose_tot we check that they all take numeric values
+    data['dose_tot'] = data['dose_tot'].apply(pd.to_numeric)
     ## For etalement we check that they all take numeric values
     data['etalement'] = data['etalement'].apply(pd.to_numeric)
-    ## For vol\n_GTV, vol\n_PTV, vol\n_ITV, couv\n_PTV, BED\n_10 we check that they all take numeric values
-    data['vol\n_GTV'] = data['vol\n_GTV'].apply(pd.to_numeric)
-    data['vol\n_PTV'] = data['vol\n_PTV'].apply(pd.to_numeric)
-    data['vol\n_ITV'] = data['vol\n_ITV'].apply(pd.to_numeric)
-    data['couv\n_PTV'] = data['couv\n_PTV'].apply(pd.to_numeric)
-    data['BED\n_10'] = data['BED\n_10'].apply(pd.to_numeric)
+    ## For vol_GTV, vol_PTV, vol_ITV, couv_PTV, BED_10 we check that they all take numeric values
+    data['vol_GTV'] = data['vol_GTV'].apply(pd.to_numeric)
+    data['vol_PTV'] = data['vol_PTV'].apply(pd.to_numeric)
+    data['vol_ITV'] = data['vol_ITV'].apply(pd.to_numeric)
+    data['couv_PTV'] = data['couv_PTV'].apply(pd.to_numeric)
+    data['BED_10'] = data['BED_10'].apply(pd.to_numeric)
 
     ## For DC we check that they all take numeric values
     data['DC'] = data['DC'].apply(pd.to_numeric)
@@ -102,19 +122,19 @@ def main():
     data['DDD'] = data['DDD'].apply(pd.to_datetime, dayfirst=True)
     ## For cause_DC we check that they all take numeric values
     data['cause_DC'] = data['cause_DC'].apply(pd.to_numeric)
-    ## For Date_R\n_PTV, Date_R\n_homo, Date_R\n_med, Date_R\n_contro, Date_R\n_horspoum we check they are all dates
+    ## For Date_R_PTV, Date_R_homo, Date_R_med, Date_R_contro, Date_R_horspoum we check they are all dates
     ## For each of these dates we build a column if takes 1 if there is a date and 0 otherwise
-    data['rechute_PTV'] = data['Date_R\n_PTV'].apply(lambda x: 1 if pd.notnull(x) else 0)
-    data['Date_R\n_PTV'] = data['Date_R\n_PTV'].apply(pd.to_datetime, dayfirst=True)
-    data['rechute_homo'] = data['Date_R\n_homo'].apply(lambda x: 1 if pd.notnull(x) else 0)
-    data['Date_R\n_homo'] = data['Date_R\n_homo'].apply(pd.to_datetime, dayfirst=True)
-    data['rechute_med'] = data['Date_R\n_med'].apply(lambda x: 1 if pd.notnull(x) else 0)
-    data['Date_R\n_med'] = data['Date_R\n_med'].apply(pd.to_datetime, dayfirst=True)
-    data['rechute_contro'] = data['Date_R\n_contro'].apply(lambda x: 1 if pd.notnull(x) else 0)
-    data['Date_R\n_contro'] = data['Date_R\n_contro'].apply(pd.to_datetime, dayfirst=True)
-    data['rechute_horspoum'] = data['Date_R\n_horspoum'].apply(lambda x: 1 if pd.notnull(x) else 0)
-    data['Date_R\n_horspoum'] = data['Date_R\n_horspoum'].replace('1', np.nan)
-    data['Date_R\n_horspoum'] = data['Date_R\n_horspoum'].apply(pd.to_datetime, dayfirst=True)
+    data['rechute_PTV'] = data['Date_R_PTV'].apply(lambda x: 1 if pd.notnull(x) else 0)
+    data['Date_R_PTV'] = data['Date_R_PTV'].apply(pd.to_datetime, dayfirst=True)
+    data['rechute_homo'] = data['Date_R_homo'].apply(lambda x: 1 if pd.notnull(x) else 0)
+    data['Date_R_homo'] = data['Date_R_homo'].apply(pd.to_datetime, dayfirst=True)
+    data['rechute_med'] = data['Date_R_med'].apply(lambda x: 1 if pd.notnull(x) else 0)
+    data['Date_R_med'] = data['Date_R_med'].apply(pd.to_datetime, dayfirst=True)
+    data['rechute_contro'] = data['Date_R_contro'].apply(lambda x: 1 if pd.notnull(x) else 0)
+    data['Date_R_contro'] = data['Date_R_contro'].apply(pd.to_datetime, dayfirst=True)
+    data['rechute_horspoum'] = data['Date_R_horspoum'].apply(lambda x: 1 if pd.notnull(x) else 0)
+    data['Date_R_horspoum'] = data['Date_R_horspoum'].replace('1', np.nan)
+    data['Date_R_horspoum'] = data['Date_R_horspoum'].apply(pd.to_datetime, dayfirst=True)
     ## For Reponse we check thay all take numeric values
     data['Reponse'] = data['Reponse'].apply(pd.to_numeric)
 
