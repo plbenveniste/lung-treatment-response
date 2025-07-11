992aef2df670bb29710401dfcbca6e2751c45119
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-07-11 11:48:48 -0400

    started editing clini model

--- model_training_review/train_clini_model.py
+++ model_training_review/train_clini_model.py
@@ -25,7 +25,6 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Preprocess the data from the lung cancer response dataset')
     parser.add_argument('--input', type=str, help='Path to the csv file containing the merged data')
     parser.add_argument('--output', type=str, help='Path to the folder where the results will be saved')
-    parser.add_argument('--test-set', type=str, default='L', help='Letter corresponding to the test set (default: L) (possible values are [D, H, L, R, T, V])')
     return parser.parse_args()
 
 
@@ -43,11 +42,8 @@ def main():
     # If the output_folder doesn't exist create it
     os.makedirs(output_folder, exist_ok=True)
 
-    test_set_letter = args.test_set
-    # Check that the test set value is possible
-    if test_set_letter not in ['D', 'H', 'L', 'R', 'T', 'V']:
-        raise ValueError(f"Test set value {test_set_letter} is not valid. Possible values are ['D', 'H', 'L', 'R', 'T', 'V']")
-
+    test_set_letter = "D"
+    
     ########################################################################
     #################### DATA PREPROCESSING ################################
     ########################################################################
@@ -105,8 +101,8 @@ def main():
     # Print the list of all column names:
     print(list(x_train.columns))
 
-    # We only keep the clinical columns : ['sexe', 'age', 'BMI', 'score_charlson', 'OMS', 'tabac', 'tabac_PA', 'tabac_sevre', 'histo', 'T', 'centrale']
-    clinical_columns = ['sexe', 'age', 'BMI', 'score_charlson', 'OMS', 'tabac', 'tabac_PA', 'tabac_sevre', 'histo', 'T', 'centrale']
+    # We only keep the clinical columns : ['sexe', 'age', 'BMI', 'score_charlson', 'OMS', 'tabac', 'tabac_PA', 'tabac_sevre', 'histo', 'T', 'centrale', 'dyspnee_NYHA']
+    clinical_columns = ['sexe', 'age', 'BMI', 'score_charlson', 'OMS', 'tabac', 'tabac_PA', 'tabac_sevre', 'histo', 'T', 'centrale', 'dyspnee_NYHA']
     x_train = x_train[clinical_columns]
     x_test = x_test[clinical_columns]
 
@@ -140,7 +136,7 @@ def main():
         # 'reg_lambda': Real(0, 1),
     }
     # We define the model
-    model = XGBClassifier(seed=42)
+    model = XGBClassifier(seed=44, scale_pos_weight=5)
     # We define the search
     search = BayesSearchCV(model, search_spaces, n_iter=100, n_jobs=1, cv=3, random_state=42, scoring='roc_auc')
     # We fit the search
@@ -150,6 +146,21 @@ def main():
     print(search.best_params_)
     # We evaluate the model
     model = search.best_estimator_
+
+    # On the training set
+    y_train_pred = model.predict(x_train)
+    y_train_proba = model.predict_proba(x_train)[:, 1]
+
+    # Compute ROC-AUC, accuracy score, Brier score and PR-AUC score
+    print("Model performance on the training set")
+    print("ROC AUC Score:", roc_auc_score(y_train, y_train_proba))
+    print("Brier score:", brier_score_loss(y_train, y_train_proba))
+    print("Average precision:", precision_score(y_train, y_train_pred))
+    print("Average Recall:", recall_score(y_train, y_train_pred))
+    print("Accuracy Score:", accuracy_score(y_train, y_train_pred))
+    precision_train, recall_train, _ = precision_recall_curve(y_train, y_train_pred)
+    print("AUC-PR score:", auc(recall_train, precision_train))
+    print("\n")
 
     # Performance on the training set
     y_test_pred = model.predict(x_test)
