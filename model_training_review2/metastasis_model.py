38acf3b089c9086a382205cb19bcaf35e808ea8b
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-11-04 11:15:59 -0500

    split primitive and metastasis in 2 files

--- model_training_review2/metastasis_model.py
+++ model_training_review2/metastasis_model.py
@@ -1,6 +1,6 @@
 """
 In this file we investigate the training of a model to predict survival in the following cases: 
-    - for primitive patients
+    - for metastasis patients
 
 Args:
     --merged-data: Path to the csv file containing the merged data
@@ -10,7 +10,7 @@ Returns:
     None
 
 Example:
-    python model_training/primitive_model.py --merged-data data/merged_data.csv --output-dir models/
+    python model_training/primitif_vs_metastase_model.py --merged-data data/merged_data.csv --output-dir models/
 
 Author: Pierre-Louis Benveniste
 """
@@ -64,7 +64,7 @@ def main():
     os.makedirs(output_folder, exist_ok=True)
 
     # Clear the log file
-    log_file = os.path.join(output_folder, f'primitive.txt')
+    log_file = os.path.join(output_folder, f'metastasis.txt')
     with open(log_file, 'w') as f:
         f.write('')
     logger.add(log_file)
@@ -100,19 +100,19 @@ def main():
     logger.info("\n")
 
     ################################################################################################
-    ######## FIRST SCENARIO : MODEL FOR PREDICTION OF SURVIVAL FOR PRIMITIVE PATIENTS ##############
+    ######## MODEL FOR PREDICTION OF SURVIVAL FOR METASTASIS PATIENTS ############
     ################################################################################################
 
-    logger.info(" ------------- Model for prediction of survival for primitive patients -------------")
+    logger.info(" ------------- Model for prediction of survival for metastasis patients -------------")
 
-    # We keep primitive patients
-    data_primitive = data_grouped[data_grouped['primitif'] == 1]
-    logger.info(f"Number of primitive patients: {data_primitive.shape[0]}")
-    logger.info(f"Number of features: {data_primitive.shape[1]}")
+    # We keep patients that are metastasis
+    data_metastasis = data_grouped[data_grouped['primitif'] != 1]
+    logger.info(f"Number of metastasis patients: {data_metastasis.shape[0]}")
+    logger.info(f"Number of features: {data_metastasis.shape[1]}")
 
     # Split into features and target
-    y = data_primitive[['DC']]
-    x = data_primitive.drop(columns=['DC', 'delai_fin_DC', 'subject_id'])
+    y = data_metastasis[['DC']]
+    x = data_metastasis.drop(columns=['DC', 'delai_fin_DC', 'subject_id'])
     
     # In this case, because we are only interested in the prediction of survival, we extract only the 'DC'
     y = y[['DC']]
@@ -126,23 +126,13 @@ def main():
     logger.info(f"Feature columns: {list(x.columns)}")
 
     # Plot the distribution of 'delai_fin_DC'
-    data_primitive['delai_fin_DC'].hist()
+    data_metastasis['delai_fin_DC'].hist()
     plt.title('Distribution of survival time between end of treatment and\n death (in days) for those that died')
-    plt.xlabel('Survival time of primitive patients')
+    plt.xlabel('Survival time of metastasis patients')
     plt.ylabel('Number of subjects')
-    plt.savefig(os.path.join(output_folder, "survival_time_distribution_primitive_patients.png"))
+    plt.savefig(os.path.join(output_folder, "survival_time_distribution_metastasis_patients.png"))
     plt.close()
 
-    # We perform bayesian hyperparameter tuning
-    # I used this blog to build it: https://xgboosting.com/most-important-xgboost-hyperparameters-to-tune/
-    search_spaces = {
-        'max_depth': Integer(3, 10), # Lower values prevent overfitting
-        'min_child_weight': Integer(1, 10), # Higher values prevent overfitting # Suggested to go as high as 5 by LeChat
-        'subsample': Real(0.5, 1), # Lower values prevent overfitting
-        'colsample_bytree': Real(0.001, 1), # Lower values prevent overfitting # Suggested to go as low as 0.5 by LeChat
-        'learning_rate': Real(0.01, 0.5, prior='log-uniform'),
-        'scale_pos_weight': Integer(2, 8), # To handle unbalanced classes
-    }
     X = x
     y = y
     # === Définition des folds externes (évaluation) ===
@@ -198,6 +188,7 @@ def main():
 
         # Feature importances
         feature_importances.append(best_model.feature_importances_)
+        break
 
     # === Résultats globaux (moyenne ± std sur les folds externes) ===
     results_df = pd.DataFrame(outer_results)
@@ -316,11 +307,10 @@ def main():
     plt.ylabel('True probability')
     plt.title('Calibration curves for each fold')
     plt.legend()
-    plt.savefig(os.path.join(output_folder, 'calibration_curves_folds_primitive.png'))
+    plt.savefig(os.path.join(output_folder, 'calibration_curves_folds_metastasis.png'))
     plt.close()
 
     # Define a common set of x-values (predicted probabilities) for interpolation
-    ## It is between min_value of prob_pred and max_value of prob_pred
     x_common = np.linspace(min(prob_pred), max(prob_pred), 100)
 
     # Interpolate each curve to the common x-values
@@ -341,16 +331,9 @@ def main():
     plt.ylabel('True probability')
     plt.title('Mean Calibration curve across folds')
     plt.legend()
-    plt.savefig(os.path.join(output_folder, 'mean_calibration_curve_primitive.png'))
+    plt.savefig(os.path.join(output_folder, 'mean_calibration_curve_metastasis.png'))
     plt.close()
 
     # Need to remove some stuff to free memory
     del base_model, search, best_model, X_train, X_test, y_train, y_test, X_selected
-    gc.collect()
-
-    return None
-
-   
-
-if __name__ == '__main__':
-    main()
+    gc.collect()    
